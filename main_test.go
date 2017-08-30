5ce5b24474124a02c12353c70b3bdb652aa2e3c5
James Mannion <mannion007@gmail.com>
2017-08-30 12:28:43 +0100

    Remove usage of fmt in test

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import "testing"
-import "fmt"
 
 func TestPlainTextStatementForSingleRegularMovieForMultipleDays(t *testing.T) {
     c := Customer{Name: "Bert Sampson"}
@@ -9,7 +8,12 @@ func TestPlainTextStatementForSingleRegularMovieForMultipleDays(t *testing.T) {
     
     var expected string = "Rental Record for Bert Sampson\n\tSome Regular Movie\t2.00\nAmount owed is 2.00\nYou earned 1 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForMultipleRegularMoviesForOneDay(t *testing.T) {
@@ -18,7 +22,12 @@ func TestPlainTextStatementForMultipleRegularMoviesForOneDay(t *testing.T) {
     
     var expected string = "Rental Record for Bert Sampson\n\tSome Regular Movie\t6.50\nAmount owed is 6.50\nYou earned 1 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForMultipleRegularMoviesForMultipleDays(t *testing.T) {
@@ -28,7 +37,12 @@ func TestPlainTextStatementForMultipleRegularMoviesForMultipleDays(t *testing.T)
     
     var expected string = "Rental Record for Bert Sampson\n\tSome Regular Movie\t6.50\n\tAnother Regular Movie\t8.00\nAmount owed is 14.50\nYou earned 2 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForSingleNewReleaseMovieForOneDay(t *testing.T) {
@@ -37,7 +51,12 @@ func TestPlainTextStatementForSingleNewReleaseMovieForOneDay(t *testing.T) {
     
     var expected string = "Rental Record for Bert Sampson\n\tSome New Release Movie\t3.00\nAmount owed is 3.00\nYou earned 1 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForSingleNewReleaseMovieForMultipleDays(t *testing.T) {
@@ -46,7 +65,12 @@ func TestPlainTextStatementForSingleNewReleaseMovieForMultipleDays(t *testing.T)
     
     var expected string = "Rental Record for Bert Sampson\n\tSome New Release Movie\t15.00\nAmount owed is 15.00\nYou earned 2 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForMultipleNewReleaseMovieForOneDay(t *testing.T) {
@@ -56,7 +80,12 @@ func TestPlainTextStatementForMultipleNewReleaseMovieForOneDay(t *testing.T) {
     
     var expected string = "Rental Record for Bert Sampson\n\tSome New Release Movie\t3.00\n\tAnother New Release Movie\t3.00\nAmount owed is 6.00\nYou earned 2 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForMultipleNewReleaseMovieForMultipleDays(t *testing.T) {
@@ -66,7 +95,12 @@ func TestPlainTextStatementForMultipleNewReleaseMovieForMultipleDays(t *testing.
     
     var expected string = "Rental Record for Bert Sampson\n\tSome New Release Movie\t15.00\n\tAnother New Release Movie\t15.00\nAmount owed is 30.00\nYou earned 4 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForSingleChildrenRentalForOneDay(t *testing.T) {
@@ -75,7 +109,12 @@ func TestPlainTextStatementForSingleChildrenRentalForOneDay(t *testing.T) {
     
     var expected string = "Rental Record for Bert Sampson\n\tSome Children's Movie\t1.50\nAmount owed is 1.50\nYou earned 1 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func testPlainTextStatementForSingleChildrenRentalForMultipleDays(t *testing.T) {
@@ -84,7 +123,12 @@ func testPlainTextStatementForSingleChildrenRentalForMultipleDays(t *testing.T) 
     
     var expected string = "Rental Record for Bert Sampson\n\tSome Children's Movie\t4.50\nAmount owed is 4.50\nYou earned 1 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForMultipleChildrenRentalForOneDay(t *testing.T) {
@@ -94,7 +138,12 @@ func TestPlainTextStatementForMultipleChildrenRentalForOneDay(t *testing.T) {
     
     var expected string = "Rental Record for Bert Sampson\n\tSome Children's Movie\t1.50\n\tAnother Children's Movie\t1.50\nAmount owed is 3.00\nYou earned 2 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForMultipleChildrenRentalForMultipleDays(t *testing.T) {
@@ -104,7 +153,12 @@ func TestPlainTextStatementForMultipleChildrenRentalForMultipleDays(t *testing.T
     
     var expected string = "Rental Record for Bert Sampson\n\tSome Children's Movie\t4.50\n\tAnother Children's Movie\t4.50\nAmount owed is 9.00\nYou earned 2 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForACombinationOfRentalsForOneDay(t *testing.T) {
@@ -115,7 +169,12 @@ func TestPlainTextStatementForACombinationOfRentalsForOneDay(t *testing.T) {
     
     var expected string = "Rental Record for Bert Sampson\n\tSome Regular Movie\t2.00\n\tSome New Release Movie\t3.00\n\tSome Children's Movie\t1.50\nAmount owed is 6.50\nYou earned 3 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
+    
+    if actual != expected {
+        t.Error("Incorrect statement")
+        t.Log(expected)
+        t.Log(actual)
+    }
 }
 
 func TestPlainTextStatementForACombinationOfRentalsForMultipleDays(t *testing.T) {
@@ -126,13 +185,10 @@ func TestPlainTextStatementForACombinationOfRentalsForMultipleDays(t *testing.T)
     
     var expected string = "Rental Record for Bert Sampson\n\tSome Regular Movie\t14.00\n\tSome New Release Movie\t30.00\n\tSome Children's Movie\t12.00\nAmount owed is 56.00\nYou earned 4 frequent renter points"
     var actual string = c.ProduceStatement().Output
-    assertStatementIsCorrect(t, expected, actual)
-}
-
-func assertStatementIsCorrect(t *testing.T, expected string, actual string) {
+    
     if actual != expected {
         t.Error("Incorrect statement")
-        fmt.Println(expected)
-        fmt.Println(actual)
+        t.Log(expected)
+        t.Log(actual)
     }
 }
